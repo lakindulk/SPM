6f7663270826da18adb8f2c86bc62a7e0ac2bbc0
MiyuMasakorala <68710740+MiyuMasakorala@users.noreply.github.com>
2021-09-25 11:20:20 -0900

    Updated contact us form validations.

--- SPM_correct/frontend/src/Screen/ContactUs.js
+++ SPM_correct/frontend/src/Screen/ContactUs.js
@@ -22,16 +22,16 @@ function ContactUs() {
             emailErr.nonAt = "Cannot insert email without @ sign"
             isValid = false;
         }
-        
+
         setEmailErr(emailErr);
-        
+
         return isValid;
     }
 
     const addContactmsg = async () => {
 
         const isValid = formValidation();
-        if(isValid){
+        if (isValid) {
             let msgObject = {
                 fullname,
                 email,
@@ -39,123 +39,126 @@ function ContactUs() {
             };
             await axios
                 .post("http://localhost:6500/contactus/create", msgObject)
-                
+
                 .then(response => {
                     console.log("done");
                     alert("Contact message sent successfully");
                 })
                 .catch(error => {
-    
+
                     alert("ERROR! " + error);
                 });
         }
-        
+
     };
-    
+
 
     return (
         <div><td>
             <tr>
-            <div className='hello'>How can we help? </div>
+                <div className='hello'>How can we help? </div>
             </tr>
 
             <div className='textContainleft'>
-            <tr>
-                
-                <div>
-                <h4>Please contact us below</h4>
-                </div>
-                
-            </tr>
+                <tr>
+
+                    <div>
+                        <h4>Please contact us below</h4>
+                    </div>
+
+                </tr>
             </div>
-            
+
             <tr>
-            <div className='formContain'>
+                <div className='formContain'>
 
                     <Form onSubmit={addContactmsg}>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Full Name</Form.Label>
-                            <Form.Control 
-                            type="text" 
-                            placeholder="Enter your full name" 
-                            value={fullname}
-                            onChange={(e) => setfullname(e.target.value)}
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your full name"
+                                value={fullname}
+                                onChange={(e) => setfullname(e.target.value)}
+                                required
                             />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Email address</Form.Label>
 
-                            <Form.Control 
-                            type="email" 
-                            placeholder="name@example.com" 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            <Form.Control
+                                type="email"
+                                placeholder="name@example.com"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
                             />
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
                             {Object.keys(emailErr).map((key) => {
-                                    return <div style={{ color: "red" }}>{emailErr[key]}</div>
-                                })}
+                                return <div style={{ color: "red" }}>{emailErr[key]}</div>
+                            })}
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                             <Form.Label>Enter your message</Form.Label>
-                            <Form.Control 
-                            as="textarea" 
-                            rows={3}
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                required
                             />
                         </Form.Group>
                         <button className='btnsub' variant="primary" type="submit">
                             Submit
                         </button>
                     </Form>
-                    </div>
+                </div>
             </tr>
-            
+
         </td>
             <td>
                 <div className='textContainright'>
-                <tr>
-                    <h4>We would love to hear from you..</h4><br/>
-                    
-                    <button className='bynfd'>Give Your Feedback</button>
-                </tr>
+                    <tr>
+                        <h4>We would love to hear from you..</h4><br />
+
+                        <button className='bynfd'>Give Your Feedback</button>
+                    </tr>
                 </div>
                 <div className='containimages'>
-                <tr>
-                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={image1}  class="d-block w-100" alt="Get your services"/>
-    </div>
-    <div class="carousel-item">
-      <img src={image2} class="d-block w-100" alt="Happy to help"/>
-    </div>
-    <div class="carousel-item">
-      <img src={image3} class="d-block w-100" alt="For healthy life"/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-                </tr>   
-                </div>     
+                    <tr>
+                        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src={image1} class="d-block w-100" alt="Get your services" />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src={image2} class="d-block w-100" alt="Happy to help" />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src={image3} class="d-block w-100" alt="For healthy life" />
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </tr>
+                </div>
             </td>
-                    </div>
-                    )
+        </div>
+    )
 }
 
-                    export default ContactUs
+export default ContactUs
