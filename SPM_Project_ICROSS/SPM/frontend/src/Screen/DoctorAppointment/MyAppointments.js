b3e1b2a8d7c677a8570b992f7a4777791ffe287d
Chathuni Kaveesha Amarasinghe <68693280+chathuni123@users.noreply.github.com>
2021-09-30 16:10:30 +0530

    Third commit

--- SPM_Project_ICROSS/SPM/frontend/src/Screen/DoctorAppointment/MyAppointments.js
+++ SPM_Project_ICROSS/SPM/frontend/src/Screen/DoctorAppointment/MyAppointments.js
@@ -1,15 +1,17 @@
 import { Component } from "react";
-import { Button, Form, Dropdown,Col, Modal, Table } from "react-bootstrap";
+import { Button, Form, Dropdown, Col, Modal, Table, OverlayTrigger, Tooltip } from "react-bootstrap";
 import "./MyAppointment.css"
 import { DatePickerComponent, TimePickerComponent } from "@syncfusion/ej2-react-calendars";
 import axios from "axios";
 import decode from "jwt-decode";
-
+import { saveAs } from 'file-saver';
+import { Link } from "react-router-dom";
 
 class MyAppointments extends Component {
 
     state = {
         id: '',
+        userId: '',
         Modal: false,
         appointmentDate: '',
         appointmentTime: '',
@@ -18,7 +20,10 @@ class MyAppointments extends Component {
         fullname: '',
         appointmentNote: '',
         selectedAppointment: '',
-        appointments: []
+        appointments: [],
+        doctors: [],
+        tempdate: '',
+        temptime: ''
     };
 
     componentDidMount = async () => {
@@ -44,10 +49,24 @@ class MyAppointments extends Component {
                 console.log(this.state.appointments);
             }
         })
+
+        // getting details of doctors
+        axios.get('http://localhost:6500/patient/doctors').then(res => {
+            if (res.data.success) {
+                this.setState({ doctors: res.data.doctors })
+                console.log(this.state.doctors)
+            } else {
+                alert(res.data.message);
+            }
+        })
     }
 
     openModal = async (appoinment) => {
-        this.setState({selectedAppointment:appoinment})
+        this.setState({ id: appoinment._id })
+        this.setState({ tempdate: appoinment.appointmentDate })
+        this.setState({ temptime: appoinment.appointmentTime })
+        this.setState({ physician: appoinment.physician })
+        this.setState({ appointmentNote: appoinment.appointmentNote })
         this.setState({ Modal: true })
     }
     closeModal = () => {
@@ -80,47 +99,121 @@ class MyAppointments extends Component {
     }
 
     Appointment = () => {
-        const appointment = {
-            appointmentDate: this.state.selectedAppointment.appointmentDate,
-            appointmentTime: this.state.selectedAppointment.appointmentTime,
-            physician: this.state.selectedAppointment.physician,
-            gender: this.state.selectedAppointment.gender,
-            userId: this.state.selectedAppointment.userId,
-            fullname: this.state.selectedAppointment.fullname,
-            appointmentNote: this.state.selectedAppointment.appointmentNote
 
-        }
-        const config = {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        if (this.state.appointmentDate == '' && this.state.appointmentTime == '') {
+            const appointment = {
+                appointmentDate: this.state.tempdate,
+                appointmentTime: this.state.temptime,
+                physician: this.state.physician,
+                id: this.state.id,
+                appointmentNote: this.state.appointmentNote
             }
+
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+                }
+            }
+
+            // update appointments
+            axios.put('http://localhost:6500/codebusters/api/patientpvt/appointment/updateapointments', appointment, config).then(res => {
+                if (res.data.success) {
+                    alert(res.data.message);
+                    window.location.reload(false);
+                }
+            });
         }
 
-        // update appointments
-        axios.put('http://localhost:6500/codebusters/api/patientpvt/appointment/updateapointments', appointment, config).then(res => {
-            if (res.data.success) {
-                alert(res.data.message);
-                window.location.reload(false);
+        else if (this.state.appointmentDate != '' && this.state.appointmentTime == '') {
+            const appointment = {
+                appointmentDate: this.state.appointmentDate.toLocaleDateString(),
+                appointmentTime: this.state.temptime,
+                physician: this.state.physician,
+                id: this.state.id,
+                appointmentNote: this.state.appointmentNote
             }
-        });
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+                }
+            }
+
+            // update appointments
+            axios.put('http://localhost:6500/codebusters/api/patientpvt/appointment/updateapointments', appointment, config).then(res => {
+                if (res.data.success) {
+                    alert(res.data.message);
+                    window.location.reload(false);
+                }
+            });
+        }
+
+        else if (this.state.appointmentDate == '' && this.state.appointmentTime != '') {
+            const appointment = {
+                appointmentDate: this.state.tempdate,
+                appointmentTime: this.state.appointmentTime.toLocaleTimeString(),
+                physician: this.state.physician,
+                id: this.state.id,
+                appointmentNote: this.state.appointmentNote
+
+            }
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+                }
+            }
+
+            // update appointments
+            axios.put('http://localhost:6500/codebusters/api/patientpvt/appointment/updateapointments', appointment, config).then(res => {
+                if (res.data.success) {
+                    alert(res.data.message);
+                    window.location.reload(false);
+                }
+            });
+        }
+
+        else {
+            const appointment = {
+                appointmentDate: this.state.appointmentDate.toLocaleDateString(),
+                appointmentTime: this.state.appointmentTime.toLocaleTimeString(),
+                physician: this.state.physician,
+                id: this.state.id,
+                appointmentNote: this.state.appointmentNote
+
+            }
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+                }
+            }
+
+            // update appointments
+            axios.put('http://localhost:6500/codebusters/api/patientpvt/appointment/updateapointments', appointment, config).then(res => {
+                if (res.data.success) {
+                    alert(res.data.message);
+                    window.location.reload(false);
+                }
+            });
+        }
+
+
     }
-             
-    
+
+
     // delete appointments
     onDelete = async (id) => {
         console.log(id);
         try {
-            await axios.delete(`http://localhost:6500/codebusters/api/patientpvt/appointment/deleteapointments/${id}`)
-                .then((res) => {
-                    if(window.confirm('Are you sure to delete this record?')){
-                    alert("Successfully Deleted");
-                    }
-                    window.location.reload();
-                })
-                
-                .catch((err) => {
-                    alert("Error occurred" + err);
-                });
+            var option = window.confirm("Are you sure you want to delete this appointment ? ")
+            if (option) {
+                await axios.delete(`http://localhost:6500/codebusters/api/patientpvt/appointment/deleteapointments/${id}`)
+                    .then((res) => {
+                        alert("Appointment Successfully Deleted");
+                        window.location.reload(true);
+                    })
+                    .catch((err) => {
+                        alert("Error occurred" + err);
+                    });
+            }
         } catch (error) {
             alert("Error occurred" + error);
         }
@@ -133,10 +226,10 @@ class MyAppointments extends Component {
         )
         this.setState({ appointments: result })
     }
+
     handleSearchArea = (e) => {
         //console.log(e.currentTarget.value);
         const searchKey = e.currentTarget.value;
-
         axios.get(`http://localhost:6500/codebusters/api/patientpvt/appointment/getapointments/${this.state.userId}`).then(res => {
             if (res.data.success) {
                 this.filterData(res.data.apointment, searchKey)
@@ -145,11 +238,38 @@ class MyAppointments extends Component {
     }
 
 
+
+    // pdf generate
+    handlepdf = async (id) => {
+        console.log(id);
+        try {
+            var option = window.confirm("Do you want a PDF version ? ")
+            if (option) {
+                await axios.post(`http://localhost:6500/patient/download_pdf/${id}`)
+                    .then(() => axios.get('http://localhost:6500/patient/download', { responseType: 'blob' }))
+                    .then((res) => {
+                        const pdfBlob = new Blob([res.data], { type: 'application/pdf' })
+                        saveAs(pdfBlob, 'Appointment.pdf')
+                    })
+                    .catch((err) => {
+                        alert("Error occurred" + err);
+                    });
+            }
+        } catch (error) {
+            alert("Error occurred" + error);
+        }
+    };
+
     render() {
         return (
             <div className="container">
                 <div className="row" >
                     <div className="col-lg-9 mt-2 mb-2">
+                        <Link to="/profile/patient/getServices">
+                            <div className="col">
+                                <Button className="btnr" variant="outline-secondary" style={{ marginLeft: "10%", width: "15%", marginTop: "20px" }}> Back</Button>
+                            </div>
+                        </Link>
                         <h3 className="myappointment-top-title" textAlign="center" style={{ marginLeft: "30%", marginTop: "35px" }}>My Appointments</h3>
                     </div>
                     <div className="input-group rounded" style={{ marginTop: "30px", marginLeft: "30%", marginBottom: "30px" }}>
@@ -174,7 +294,7 @@ class MyAppointments extends Component {
                             <thead >
                                 <tr >
                                     <th >#</th>
-                                    <th >Appointment Date</th>
+                                    <th>Appointment Date</th>
                                     <th>Appointment Time</th>
                                     <th>Physician</th>
                                     <th>Full Name</th>
@@ -194,15 +314,15 @@ class MyAppointments extends Component {
                                         <td>{appointments.appointmentNote}</td>
 
                                         <th scope="col">
-                                            <a className="btn btn-warning" href="#" style={{ marginTop: "5px" }} onClick={this.openModal.bind(this,appointments)}>
+                                            <a className="btn btn-warning" href="#" style={{ marginTop: "5px" }} onClick={this.openModal.bind(this, appointments)}>
                                                 <i className="far fa-edit" ></i>&nbsp; Edit
                                             </a>
                                             &nbsp;
-                                            <a className="btn btn-danger" href="#" style={{ marginTop: "10px" }}onClick={this.onDelete.bind(this,appointments._id)} >
+                                            <a className="btn btn-danger" href="#" style={{ marginTop: "10px" }} onClick={this.onDelete.bind(this, appointments._id)} >
                                                 <i className="far fa-trash-alt"></i>&nbsp; Delete
                                             </a>
                                             &nbsp;
-                                            <a className="btn btn-primary" href="#" style={{ marginTop: "10px" }}>
+                                            <a className="btn btn-primary" href="#" style={{ marginTop: "10px" }} onClick={this.handlepdf.bind(this, appointments._id)}>
                                                 <i className="fa fa-download"></i>&nbsp; Generate PDF
                                             </a>
                                         </th>
@@ -222,22 +342,42 @@ class MyAppointments extends Component {
                 >
                     <Modal.Body >
                         <Form>
+
                             <Form.Group className="mb-3" as={Col} md={10} controlId="formBasicDate">
-                                <Form.Label style={{ marginTop: "20px", font: " bold 20px/25px Times New Roman,serif" }}>Appointment Date</Form.Label>
+                                <Form.Label style={{ marginTop: "20px", font: " bold 20px/25px Times New Roman,serif" }}>Old Apointment Date</Form.Label>
+                                <div>
+                                    <DatePickerComponent placeholder="Enter Date"
+                                        value={this.state.tempdate} style={{ marginTop: "20px" }} >
+                                    </DatePickerComponent>
+                                </div>
+                            </Form.Group>
+
+
+
+                            <Form.Group className="mb-3" as={Col} md={10} controlId="formBasicDate">
+                                <Form.Label style={{ marginTop: "20px", font: " bold 20px/25px Times New Roman,serif" }}>New Appointment Date</Form.Label>
                                 <div>
                                     <DatePickerComponent placeholder="Enter Date"
                                         //value={this.state.selectedAppointment.appointmentDate} name="appointmentDate" format="dd - MMM - yy" style={{ marginTop: "20px" }}
-                                        value={this.state.selectedAppointment.appointmentDate} name="appointmentDate" format="dd - MMM - yy" style={{ marginTop: "20px" }}
+                                        value={this.state.appointmentDate} name="appointmentDate" format="dd - MMM - yy" style={{ marginTop: "20px" }}
                                         onChange={this.appointmentDate}>
                                     </DatePickerComponent>
                                 </div>
                             </Form.Group>
 
                             <Form.Group className="mb-3" as={Col} md={10} controlId="formBasicTime">
-                                <Form.Label style={{ marginTop: "20px", font: " bold 20px/25px Times New Roman,serif" }}>Appointment Time</Form.Label>
+                                <Form.Label style={{ marginTop: "20px", font: " bold 20px/25px Times New Roman,serif" }}>Old Appointment Time</Form.Label>
+                                <div>
+                                    <TimePickerComponent placeholder="Select a Time" value={this.state.temptime}>
+                                    </TimePickerComponent>
+                                </div>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" as={Col} md={10} controlId="formBasicTime">
+                                <Form.Label style={{ marginTop: "20px", font: " bold 20px/25px Times New Roman,serif" }}>New Appointment Time</Form.Label>
                                 <div>
                                     <TimePickerComponent placeholder="Select a Time"
-                                        value={this.state.selectedAppointment.appointmentTime} min={this.state.minTime} max={this.state.maxTime}
+                                        value={this.state.appointmentTime} min={this.state.minTime} max={this.state.maxTime}
                                         //value={this.state.appointmentTime} min={this.state.minTime} max={this.state.maxTime}
                                         name="appointmentTime"
                                         format="HH:mm" step={60}
@@ -246,24 +386,24 @@ class MyAppointments extends Component {
                                     </TimePickerComponent>
                                 </div>
                             </Form.Group>
-                            <Dropdown style={{ marginLeft: "5%", width: "90%" }} onSelect={this.selectPhysician}>
-                                <Form.Label style={{ marginTop: "10px", font: " bold 20px/20px Times New Roman,serif"}}> Physician</Form.Label>
-                                <Dropdown.Toggle variant="success" id="dropdown-basic" style={{ maxHeight: "100%", marginLeft: "12%", width: "70%" }}>
-                                    {this.state.selectedAppointment.physician}
-                                </Dropdown.Toggle>
 
-                                <Dropdown.Menu style={{ width: "100%" }}>
-                                    <Dropdown.Item eventKey="Mr. Silva">Mr. Silva</Dropdown.Item>
-                                    <Dropdown.Item eventKey="Mr. Perera">Mr. Perera</Dropdown.Item>
-                                    <Dropdown.Item eventKey="Mrs. Amarasinghe">Mrs. Amarasinghe</Dropdown.Item>
-                                    <Dropdown.Item eventKey="Mrs. Gamage">Mrs. Gamage</Dropdown.Item>
-                                    <Dropdown.Item eventKey="Dr. Namal Gamage">Dr. Namal Gamage</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
+
+                            <Form.Group className="mb-3" controlId="exampleFormage" height="30%">
+                                <Form.Label name="physician" style={{ marginTop: "10px", font: " bold 20px/20px Times New Roman,serif" }}> Physician</Form.Label>
+                                <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip-disabled" ><strong><h5>Enter the PHYSICIAN NAME is required !</h5></strong></Tooltip>}>
+                                <select class="form-select" name="physician" value={this.state.physician} onChange={this.handleChange} >
+                                    {this.state.doctors.map((e) => {
+                                        return <option key={e._id} value={e.fullname}>{e.fullname}</option>;
+                                    })}
+                                </select>
+                                </OverlayTrigger>
+                            </Form.Group>
 
                             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" height="30%">
                                 <Form.Label style={{ marginTop: "30px", font: " bold 20px/25px Times New Roman,serif" }}>Appointment Note</Form.Label>
-                                <Form.Control as="textarea" name="appointmentNote" rows={3} style={{ marginTop: "10px" }} onChange={this.appointmentNote} value={this.state.selectedAppointment.appointmentNote} />
+                                <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip-disabled"><strong><h5>Enter the NOTE is required !</h5></strong></Tooltip>}>
+                                <Form.Control as="textarea" name="appointmentNote" rows={3} style={{ marginTop: "10px" }} onChange={this.appointmentNote} value={this.state.appointmentNote} />
+                                </OverlayTrigger>
                             </Form.Group>
                         </Form>
                     </Modal.Body>
