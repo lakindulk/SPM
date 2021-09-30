1c16c27646cef71d7c2890b420c2f1e6bc849552
Chathuni Kaveesha Amarasinghe <68693280+chathuni123@users.noreply.github.com>
2021-09-30 16:01:16 +0530

    add avatar to model

--- SPM_Project_ICROSS/SPM/backend/models/patient-model.js
+++ SPM_Project_ICROSS/SPM/backend/models/patient-model.js
@@ -57,6 +57,11 @@ const PatientSchema = new mongoose.Schema({
     
   },
 
+  avatar: {
+    type: String,
+    default: 'https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png'
+  },
+
   appoinment : [
     {
       appointmentDate:{
