f327476df8a961f2e8c20b4d499c81fc124ce8aa
Chathuni Kaveesha Amarasinghe <68693280+chathuni123@users.noreply.github.com>
2021-09-30 16:03:13 +0530

    add status, telNo and upload image

--- SPM_Project_ICROSS/SPM/backend/models/medicine-order-model.js
+++ SPM_Project_ICROSS/SPM/backend/models/medicine-order-model.js
@@ -1,45 +1,59 @@
 const mongoose = require('mongoose');
 
 const MedicineOrderSchema = new mongoose.Schema({
-    name:{
-        type:String,
+    name: {
+        type: String,
         required: true,
     },
-    age:{
-        type:Number,
+    age: {
+        type: Number,
         required: true,
     },
-    email:{
-        type:String,
+    email: {
+        type: String,
         required: true,
     },
-    gender:{
-        type:String,
+    telNo: {
+        type: Number,
+
+    },
+    gender: {
+        type: String,
         required: true,
     },
-    address:{
-        type:String,
+    address: {
+        type: String,
         required: true,
     },
-    allergies:{
-        type:String,
+    allergies: {
+        type: String,
+
+    },
+    currentlyTakingMedications: {
+        type: String,
+
+    },
+    existingMedicalProblems: {
+        type: String,
+
+    },
+    userID: {
+        type: String,
+    },
+    signature: {
+        type: String,
         required: true,
     },
-    currentlyTakingMedications:{
-        type:String,
+    photo: {
+        type: String,
         required: true,
     },
-    existingMedicalProblems:{
-        type:String,
-        required: true,
-    },
-    userID:{
-        type:String,
-      },
-    signature:{
-        type:String,
-        required: true,
+
+    status: {
+        type: String,
+        default: "Pending",
     }
+
 });
 
 const MedicineOrder = mongoose.model(
